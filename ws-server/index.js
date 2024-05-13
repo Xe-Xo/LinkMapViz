f03892b837e0f0d3721aef2f928644750d4116d0
Xe-Xo <50796415+Xe-Xo@users.noreply.github.com>
2024-05-13 11:55:25 +1000

    add websocket

--- ws-server/index.js
+++ ws-server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const https = require('https');
 const WebSocketServer = require('ws').Server;
+const WebSocket = require('ws');
 
 
 console.log("Starting server");
